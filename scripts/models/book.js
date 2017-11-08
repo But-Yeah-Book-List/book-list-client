74ab01a906c7b7ab90947733b16026496fe1b0ad
Robert Reed <robert.mc.reed@gmail.com>
2017-11-08 09:54:22 -0800

    setting up for local tests

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var app = app || {};
-var __API_URL__ = 'https://sd-rr-booklist.herokuapp.com/';
+// var __API_URL__ = 'https://sd-rr-booklist.herokuapp.com';
+var __API_URL__ = 'localhost:3000';
 
 (function(module) {
-  $.get(`${__API_URL__}test`)
+  $.get(`${__API_URL__}/test`)
     .then(console.log);
 
 })(app);
