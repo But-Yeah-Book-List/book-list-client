ac9b4e60733edf67942616618f24c38fcd3b6317
Robert Reed <robert.mc.reed@gmail.com>
2017-11-08 10:04:57 -0800

    local setup working

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,7 +2,7 @@
 
 var app = app || {};
 // var __API_URL__ = 'https://sd-rr-booklist.herokuapp.com';
-var __API_URL__ = 'localhost:3000';
+var __API_URL__ = 'http://localhost:3000';
 
 (function(module) {
   $.get(`${__API_URL__}/test`)
