6f697ae538fe4fe9b34f248d4a9d9d5a9aa2f96d
Robert Reed <robert.mc.reed@gmail.com>
2017-11-08 09:48:57 -0800

    fixed test route

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -4,7 +4,7 @@ var app = app || {};
 var __API_URL__ = 'https://sd-rr-booklist.herokuapp.com/';
 
 (function(module) {
-  $.get(`${__API_URL__}/test`)
+  $.get(`${__API_URL__}test`)
     .then(console.log);
 
 })(app);
