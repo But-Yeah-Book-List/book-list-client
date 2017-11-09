a908e52db82db6c3c17a643e181df261d51e1769
Robert Reed <robert.mc.reed@gmail.com>
2017-11-09 10:36:34 -0800

    pages hotfix, maybe?

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // This is where we define the client-side routes
-
+page.base('/book-list-client');
 page('/', app.Book.fetchAll(app.bookView.initIndexPage));
 page('/error', app.errorView.initErrorPage);
 page('/books/new', app.bookView.initFormPage);
