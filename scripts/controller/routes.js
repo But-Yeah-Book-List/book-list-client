08aae223841778aff83ffcfdc22648a20822085f
Robert Reed <robert.mc.reed@gmail.com>
2017-11-10 11:56:49 -0800

    refactored to work locally and remotely without refactor

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
-page.base('/book-list-client');
+// Set the page.js base to work for deployed site
+if(location.hostname === 'but-yeah-book-list.github.io') page.base('/book-list-client');
 
 // This is where we define the client-side routes
 page('/', app.Book.fetchAll(app.bookView.initIndexPage));
