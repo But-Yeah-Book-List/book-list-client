d8bae7f952585a7ce9f62b2f1a9d63a74d11bb86
Robert Reed <robert.mc.reed@gmail.com>
2017-11-11 16:11:59 -0800

    added route for about us page

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -5,11 +5,12 @@ if(location.hostname === 'but-yeah-book-list.github.io') page.base('/book-list-c
 
 // This is where we define the client-side routes
 page('/', app.Book.fetchAll, app.bookView.initIndexPage);
-page('/error', app.errorView.initErrorPage);
 page('/books/new', app.bookView.initFormPage);
 page('/books/:book_id', app.Book.fetchOne, app.bookView.initDetailPage);
 page('/books/:book_id/update', app.Book.fetchOne, app.bookView.initUpdateFormPage);
 page('/admin', app.adminView.initAdminPage, app.adminView.setAdmin);
 page('/admin/:book_id', app.adminView.initAdminPage, app.adminView.setAdmin);
+page('/about', app.bookView.initAboutPage);
+page('/error', app.errorView.initErrorPage);
 
 page();
